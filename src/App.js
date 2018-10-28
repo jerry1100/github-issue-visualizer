a91db673eb227c274b3418cccd338246dfe79334
jerry1100 <jwu1100@gmail.com>
2018-10-27 21:42:03 -0700

    add chartjs, show all issues by date

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Scatter } from 'react-chartjs-2';
 import { fetchAllIssues } from './util/github-util';
 import './App.css';
 
@@ -9,6 +10,7 @@ class App extends Component {
     repoName: '',
     apiKey: '',
     numOpenIssues: null,
+    chartData: null,
   }
 
   handleDomainChange = event => {
@@ -35,21 +37,36 @@ class App extends Component {
       apiKey: this.state.apiKey,
     });
 
-    // Remove time portion in issue dates
-    const formattedIssues = issues.map(issue => ({
-      ...issue,
-      createdAt: new Date(issue.createdAt).toDateString(),
-      closedAt: issue.closedAt && new Date(issue.closedAt).toDateString(),
-    }));
-
     // Get number of issues by date
-    const numIssuesByDate = [...new Set(formattedIssues.map(issue => issue.createdAt))]
+    const numIssuesByDate = [...new Set(issues.map(issue => issue.createdAt))]
       .map(date => ({
-        x: new Date(date),
-        y: formattedIssues.filter(issue => new Date(date) >= new Date(issue.createdAt)).length,
+        x: date,
+        y: issues.filter(issue => new Date(date) >= new Date(issue.createdAt)).length,
       }));
 
     console.log(numIssuesByDate);
+
+    const chartData = {
+      datasets: [
+        {
+          label: 'Open Issues by Date',
+          showLine: true,
+          backgroundColor: 'rgba(75,192,192,0.4)',
+          pointBorderColor: 'rgba(75,192,192,1)',
+          pointBackgroundColor: '#fff',
+          pointBorderWidth: 1,
+          pointHoverRadius: 5,
+          pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+          pointHoverBorderColor: 'rgba(220,220,220,1)',
+          pointHoverBorderWidth: 2,
+          pointRadius: 1,
+          pointHitRadius: 10,
+          data: numIssuesByDate,
+        },
+      ],
+    };
+
+    this.setState({ chartData });
   }
 
   render() {
@@ -80,6 +97,22 @@ class App extends Component {
           <div>
             {this.state.repoOwner}/{this.state.repoName} has {this.state.numOpenIssues} open issues
           </div>
+        )}
+        {this.state.chartData && (
+          <Scatter
+            data={this.state.chartData}
+            options={{
+              scales: {
+                xAxes: [{
+                  type: 'time',
+                  time: {
+                    unit: 'day',
+                    tooltipFormat: 'lll' // https://momentjs.com/
+                  },
+                }],
+              }
+            }}
+          />
         )}
       </div>
     );
