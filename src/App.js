f4c6cd471edd0ee5d8f651fcff457c2057e244c7
jerry1100 <jwu1100@gmail.com>
2018-10-28 00:52:53 -0700

    set default repo so I don't have to keep reentering it

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 
 class App extends Component {
   state = {
-    repoURL: '',
+    repoURL: 'https://github.com/facebook/create-react-app',
     apiKey: '',
     numOpenIssues: null,
     chartData: null,
