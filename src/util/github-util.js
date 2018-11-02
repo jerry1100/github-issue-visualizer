bf0f375d168c891a4fd4319c58ea55b220d6bf5e
jerry1100 <jwu1100@gmail.com>
2018-11-01 22:19:28 -0700

    implement fetchAllIssues() using REST API

--- src/util/github-util.js
+++ src/util/github-util.js
@@ -138,3 +138,57 @@ export async function fetchLabels(options) {
     before: pageInfo.startCursor,
   })).concat(nodes);
 }
+
+/**
+ * Fetches all of a repo's issues
+ * @param {object} options Request options
+ */
+export async function fetchAllIssues(options) {
+  const { domain, owner, name, apiKey } = options;
+
+  // Get the total number of issues and PRs
+  // Note: we need to include PRs since GitHub's issues API returns BOTH issues and PRs
+  const numIssuesAndPRs = await fetch(`https://api.${domain}/graphql`, {
+    method: 'post',
+    headers: { 'Authorization': `Basic ${window.btoa(apiKey)}` },
+    body: JSON.stringify({
+      query: `query($owner: String!, $name: String!) {
+        repository(owner: $owner, name: $name, ) {
+          issues {
+            totalCount
+          }
+          pullRequests {
+            totalCount
+          }
+        }
+      }`,
+      variables: { owner, name },
+    }),
+  })
+    .then(response => response.json())
+    .then(response => !response.data ? Promise.reject(response) : response.data) // queries can fail
+    .then(({ repository }) => repository.issues.totalCount + repository.pullRequests.totalCount);
+
+  // Send requests in parallel to retrieve all the issues
+  const numPagesNeeded = Math.ceil(numIssuesAndPRs / 100);
+  const pageNumbers = [...Array(numPagesNeeded).keys()].map(index => index + 1); // [1...numPagesNeeded]
+  const results = await Promise.all(pageNumbers.map(pageNumber => (
+    fetch(`https://api.${domain}/repos/${owner}/${name}/issues?state=all&direction=asc&per_page=100&page=${pageNumber}`, {
+      headers: { 'Authorization': `Basic ${window.btoa(apiKey)}` },
+    })
+      .then(response => response.json())
+      .then(response => response.filter(data => !data.pull_request)) // filter out pull requests
+  )));
+
+  // Flatten results into a single array
+  const issues = results.reduce((total, current) => total.concat(current));
+
+  // Return only relevant properties
+  return issues.map(issue => ({
+    title: issue.title,
+    number: issue.number,
+    createdAt: issue.created_at,
+    closedAt: issue.closed_at,
+    labels: issue.labels.map(label => label.name),
+  }));
+}
